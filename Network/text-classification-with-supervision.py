53409c467461cb6e17891cb3d8753a5e95ade8cf
Mateusz Szymczak <mateuszszymczak15@gmail.com>
2019-01-05 13:39:14 +0100

    Model adjustment

--- Network/text-classification-with-supervision.py
+++ Network/text-classification-with-supervision.py
@@ -5,9 +5,9 @@ from random import shuffle
 import numpy as np
 import pandas as pd
 import sklearn.datasets as skds
-from keras.callbacks import TensorBoard
-from keras.engine.saving import model_from_json
-from keras.layers import Dense, Embedding, Conv1D, GlobalMaxPool1D, BatchNormalization, Dropout
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.models import load_model
+from keras.layers import Dense, Embedding, Conv1D, GlobalMaxPool1D, BatchNormalization, Dropout, K
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
@@ -16,18 +16,25 @@ from sklearn.preprocessing import MultiLabelBinarizer
 # Source file directory
 
 fileDir = os.path.dirname(os.path.abspath(__file__))
-parentDir = os.path.dirname(fileDir)
+# parentDir = os.path.dirname(fileDir)
+parentDir = "C:\\Users\\mateu\\Desktop\\PoprawneCalosc\\PoprawneCalosc"
 path_train = parentDir + "/Data"
 path_to_labels = parentDir + "/Tag/Labelki"
-
+cateogories = set()
 d = {}
-with open("finalCategories.txt", "r") as file:
-    for line in file:
-        key, value = line.strip().split(" ")
-        d[key] = int(value)
+# Params
+num_labels = 97
+vocab_size = 3000
+batch_size = 150
+max_len = 300
+mode = 'test'  # ['train', 'test']
+read_from_csv = False
+
 
 
 def find_categories(filename):
+
+
     text = Path(path_to_labels + "/" + filename).read_text()
     res = [int(i) for i in text.split()]
     return find_categories_with_array(res)
@@ -35,9 +42,11 @@ def find_categories(filename):
 
 def find_categories_with_array(array):
     current_text_categories = []
+    file = open("finalCategories.txt", "r")
+    all_categories = [i for i in file.read().split()]
     for idx, value in enumerate(array):
         if value == 1:
-            current_text_categories.append(list(d.keys())[idx])
+            current_text_categories.append(all_categories[idx])
 
     return current_text_categories
 
@@ -47,38 +56,76 @@ def get_weights():
     max_val = sum(keys)
     class_weights = {}
     for idx, value in enumerate(keys):
-        class_weights[idx] = (int(max_val) / int(value))
+        class_weights[idx] = (int(max_val) / (int(value) * 10))
     return class_weights
 
 
+# TODO Fmiara  Done
+# TODO maxlen test  Done
+# TODO sam abstract
+# TODO Gotowe embedingi
 def get_features(text_series):
     sequences = tokenizer.texts_to_sequences(text_series)
-    return pad_sequences(sequences, maxlen=150)
+    return pad_sequences(sequences, maxlen=max_len)
 
 
-# Params
-num_labels = 97
-vocab_size = 1500
-batch_size = 100
-mode = 'train'  # ['train', 'test']
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
 
-files_train = skds.load_files(path_train, load_content=False)
-label_index = files_train.target
-label_names = files_train.target_names
-labelled_files = files_train.filenames
+        Only computes a batch-wise average of recall.
 
-data_tags = ["filename", "Text", 'Tags']
-data_list = []
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
 
-# Read and add data from file to a list
-print("Creating dataset...")
-i = 0
-for f in labelled_files:
-    data_list.append((f, Path(f).read_text(), find_categories(os.path.basename(f))))
-    i += 1
-shuffle(data_list)
-# We have training data available as dictionary filename, category, data
-data = pd.DataFrame.from_records(data_list, columns=data_tags)
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
+
+if not read_from_csv:
+    files_train = skds.load_files(path_train, load_content=False)
+    label_index = files_train.target
+    label_names = files_train.target_names
+    labelled_files = files_train.filenames
+
+    data_tags = ["filename", "Text", 'Tags']
+    data_list = []
+
+    # Read and add data from file to a list
+    print("Preparing dataset...")
+    i = 0
+    for f in labelled_files:
+        data_list.append((f, Path(f).read_text(), find_categories(os.path.basename(f))))
+        i += 1
+    shuffle(data_list)
+    # We have training data available as dictionary filename, category, data
+    data = pd.DataFrame.from_records(data_list, columns=data_tags)
+
+    print("Saving data frame to csv...")
+    header = ["filename", "Text", "Tags"]
+    data.to_csv("data.csv", index=False, header=header)
+else:
+    data = pd.read_csv("data.csv")
+# datahehe = pd.read_csv("data.csv")
 
 print("Tokenizing...")
 train_size = int(len(data) * .8)
@@ -99,73 +146,66 @@ x_test = get_features(test_texts)
 
 multilabel_binarizer = MultiLabelBinarizer()
 multilabel_binarizer.fit(train_tags)
+print(train_tags)
 labels = multilabel_binarizer.classes_
 y_train = multilabel_binarizer.transform(train_tags)
-y_test = multilabel_binarizer.transform(test_texts)
-
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=9000)
-
-filter_length = 300
-
-model = Sequential()
-model.add(Embedding(3000, 20, input_length=150))
-# model.add(Dropout(0.1))
-model.add(Conv1D(filter_length, 3, padding='valid', activation='relu', strides=1))
-model.add(BatchNormalization())
-model.add(GlobalMaxPool1D())
-model.add(Dense(num_labels, activation='sigmoid'))
-
-# Old model
-# model = Sequential()
-# model.add(Dense(5000, activation='relu', input_dim=150))
-# model.add(Dropout(0.1))
-# model.add(Dense(600, activation='relu'))
-# model.add(Dropout(0.1))
-# model.add(GlobalMaxPool1D())
-# model.add(Dense(num_labels, activation='sigmoid'))
-
-model.compile(optimizer='adamax', loss='binary_crossentropy', metrics=['categorical_accuracy', 'accuracy'])
-
-callbacks = [
-    TensorBoard(log_dir=fileDir + "/logs/weight")
-]
-
-# TODO writing and reading model
+y_test = multilabel_binarizer.transform(test_tags)
 if mode == 'train':
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=9000)
+
+    filter_length = 300
+
+    model = Sequential()
+    model.add(Embedding(2000, 20, input_length=max_len))
+    # model.add(Dropout(0.1))
+    model.add(Conv1D(filter_length, 3, padding='valid', activation='relu', strides=1))
+    model.add(BatchNormalization())
+    model.add(GlobalMaxPool1D())
+    model.add(Dense(num_labels, activation='sigmoid'))
+
+    # Old model
+    # model = Sequential()
+    # model.add(Embedding(2000, 20, input_length=150))
+    # model.add(Dropout(0.15))
+    # model.add(GlobalMaxPool1D())
+    # model.add(Dense(num_labels, activation='sigmoid'))
+
+    model.compile(optimizer='adamax', loss='binary_crossentropy', metrics=[f1, 'categorical_accuracy', 'accuracy'])
+    tensor_board = TensorBoard(log_dir=fileDir + "/logs/conv/nope")
+    checkpoint = ModelCheckpoint('checkpointweight.hdf5', monitor='val_loss', save_best_only=True)
+    callbacks = [
+        tensor_board,
+        checkpoint
+    ]
+
+    # TODO writing and reading model
+
     history = model.fit(x_train, y_train,
-                        epochs=35,
+                        epochs=5,
                         batch_size=batch_size,
                         validation_split=0.1,
                         callbacks=callbacks
                         )
-    # serialize model to JSON
-    model_json = model.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights("model.h5")
-    print("Saved model to disk")
+    model.model.save('my_model.h5')
+    model.model.save_weights('wei.h5')
+    # Deletes the existing model
 else:
     # load json and create model
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights("model.h5")
+    model = load_model('my_model.h5', custom_objects={'f1': f1})
+    model.model.load_weights('wei.h5')
     print("Loaded model from disk")
-    loaded_model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['categorical_accuracy'])
-    score = model.evaluate(x_test, y_test,
-                           batch_size=batch_size,
-                           verbose=1
-                           )
-    print(score)
 
-    for i in range(10):
-        prediction = loaded_model.predict(np.array([x_test[i]]))
-        prediction[prediction >= 0.5] = 1
-        prediction[prediction < 0.5] = 0
-        predicted_label = find_categories_with_array(prediction[0])
-        print(test_files_names.iloc[i])
-        print(test_tags.iloc[i])
-        print(predicted_label)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size,
+                       verbose=1
+                       )
+print(score)
+
+for i in range(10):
+    prediction = model.predict(np.array([x_test[i]]))
+    prediction[prediction >= 0.5] = 1
+    prediction[prediction < 0.5] = 0
+    predicted_label = find_categories_with_array(prediction[0])
+    print(test_files_names.iloc[i])
+    print(test_tags.iloc[i])
+    print(predicted_label)
